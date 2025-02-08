5b5907946df942b3a08fff3338c82608f1ab2e31
Alexey Dyakonov <dyakonov261@gmail.com>
2025-02-08 15:56:56 +0500

    Finish

---
+++ users/management/commands/csu.py
@@ -0,0 +1,16 @@
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        user = User.objects.create(
+            email="alexey",
+        )
+        user.set_password("4451")
+        user.is_active = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
